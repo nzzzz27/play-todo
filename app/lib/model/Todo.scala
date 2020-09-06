390afecfbacd7b02cb31f58c6917916c532c1c63
Nozomi <nz.yonezawa@gmail.com>
2020-09-06 10:46:36 +0900

    [create] todo lib model

---
+++ app/lib/model/Todo.scala
@@ -0,0 +1,13 @@
+package model
+
+import java.time.LocalDateTime
+
+case class Todo(
+  id:          Option[Long],
+  body:        String,
+  note:        Option[String],
+  category_id: Long,
+  status:      Short,
+  updated_at:  LocalDateTime,
+  created_at:  LocalDateTime,
+)

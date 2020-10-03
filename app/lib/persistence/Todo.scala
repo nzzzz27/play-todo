028da3e2cd6adf27cc70dc6c293c76b61c1bb0a3
Nozomi <nz.yonezawa@gmail.com>
2020-10-03 16:50:28 +0900

    [update] delete some comments

--- app/lib/persistence/Todo.scala
+++ app/lib/persistence/Todo.scala
@@ -17,14 +17,9 @@ import play.api.db.slick.{
 }
 import slick.jdbc.JdbcProfile
 import scala.concurrent.{ Future, ExecutionContext }
-
 import lib.model.Todo
-// import lib.persistence.db.TodoTable
-
 import TodoTable.todo
-// @NOTE
-// abstract修飾子をつけるとエラーが出る。case classまたはclassなら大丈夫
-// No implementation for lib.persistence.TodoRepository was bound.
+
 class TodoRepository @Inject()(
   protected val dbConfigProvider: DatabaseConfigProvider,
 )(implicit ec: ExecutionContext)
@@ -71,43 +66,44 @@ object TodoTable {
   val todo: TableQuery[TodoTable] = TableQuery[TodoTable]
 }
 
-// @NOTE
-// slick3.3以降のの仕様で、LocalDateTimeがうまく動かないので、一旦コメントアウト
-// https://github.com/nzzzz27/play-todo/pull/21#discussion_r495432997
-// class TodoTable(tag: Tag) extends Table[Todo](tag, "todo") {
-//
-//   // Columns
-//   /* @1 */ def id          = column[Long]           ("ID", O.PrimaryKey, O.AutoInc)
-//   /* @2 */ def body        = column[String]         ("BODY")
-//   /* @3 */ def note        = column[Option[String]] ("NOTE")
-//   /* @4 */ def category_id = column[Long]           ("CATEGORY_ID")
-//   /* @5 */ def status      = column[Short]          ("STATUS")
-//   /* @6 */ def updated_at  = column[LocalDateTime]  ("UPDATED_AT")
-//   /* @7 */ def created_at  = column[LocalDateTime]  ("CREATED_AT")
-//
-//   type TableElementTuple = (
-//     Option[Long],
-//     String,
-//     Option[String],
-//     Long,
-//     Short,
-//     LocalDateTime,
-//     LocalDateTime,
-//   )
-//
-//   // DB <=> Scala の相互のmapping定義
-//   def * = (id.?, body, note, category_id, status, updated_at, created_at) <> (
-//     // Tuple(table) => Model
-//     (t: TableElementTuple) => Todo(
-//       t._1, t._2, t._3, t._4, t._5, t._6, t._7
-//     ),
-//     // Model => Tuple(table)
-//     (v: Todo) => Todo.unapply(v).map { t => (
-//       t._1, t._2, t._3, t._4, t._5, LocalDateTime.now(), t._7
-//     )}
-//   )
-//
-//   // TodoTableへアクセスできるようにする
-//   val todo: TableQuery[TodoTable] = TableQuery[TodoTable]
-// }
-//
+/*
+ @NOTE
+ slick3.3以降のの仕様で、LocalDateTimeがうまく動かないので、一旦コメントアウト
+ https://github.com/nzzzz27/play-todo/pull/21#discussion_r495432997
+ class TodoTable(tag: Tag) extends Table[Todo](tag, "todo") {
+
+   // Columns
+   /* @1 */ def id          = column[Long]           ("ID", O.PrimaryKey, O.AutoInc)
+   /* @2 */ def body        = column[String]         ("BODY")
+   /* @3 */ def note        = column[Option[String]] ("NOTE")
+   /* @4 */ def category_id = column[Long]           ("CATEGORY_ID")
+   /* @5 */ def status      = column[Short]          ("STATUS")
+   /* @6 */ def updated_at  = column[LocalDateTime]  ("UPDATED_AT")
+   /* @7 */ def created_at  = column[LocalDateTime]  ("CREATED_AT")
+
+   type TableElementTuple = (
+     Option[Long],
+     String,
+     Option[String],
+     Long,
+     Short,
+     LocalDateTime,
+     LocalDateTime,
+   )
+
+   // DB <=> Scala の相互のmapping定義
+   def * = (id.?, body, note, category_id, status, updated_at, created_at) <> (
+     // Tuple(table) => Model
+     (t: TableElementTuple) => Todo(
+       t._1, t._2, t._3, t._4, t._5, t._6, t._7
+     ),
+     // Model => Tuple(table)
+     (v: Todo) => Todo.unapply(v).map { t => (
+       t._1, t._2, t._3, t._4, t._5, LocalDateTime.now(), t._7
+     )}
+   )
+
+   // TodoTableへアクセスできるようにする
+   val todo: TableQuery[TodoTable] = TableQuery[TodoTable]
+ }
+*/

a1e3d9f4b07401be18550a7a0458fea97c90ee8c
Nozomi <nz.yonezawa@gmail.com>
2020-10-15 08:46:28 +0900

    [update] sql

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -4,6 +4,7 @@
 CREATE TABLE category (
   id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   name varchar(255) NOT NULL,
+  color tinyint unsigned,
   updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (id)
--- conf/evolutions/default/3.sql
+++ conf/evolutions/default/3.sql
@@ -4,18 +4,15 @@ INSERT INTO todo (body, note, category_id, status)
   VALUES ("掃除", "お風呂の掃除をする", 1, 1);
 
 INSERT INTO todo (body, note, category_id, status) 
-  VALUES ("料理", "クリスマスのメニューを考える", 2, 0);
+  VALUES ("料理", "クリスマスのメニューを考える", 2, 3);
 
 INSERT INTO todo (body, note, category_id, status)
-  VALUES ("料理", "ケーキを作る", 3, 0);
+VALUES ("料理", "クリスマスのメニューを考える", 2, 2);
 
-INSERT INTO category (name) 
-  VALUES ("優先度：高");
+INSERT INTO category (name, color)
+  VALUES ("優先度：高", 1);
 
-INSERT INTO category (name) 
-  VALUES ("優先度：中");
-
-INSERT INTO category (name) 
-  VALUES ("優先度：低");
+INSERT INTO category (name, color)
+  VALUES ("優先度：中", 2);
 
 # --- !Downs

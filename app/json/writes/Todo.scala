614cbde9c42f43cbfade2575bacccded490b0f52
Nozomi <nz.yonezawa@gmail.com>
2020-10-16 07:47:13 +0900

    [WIP] update json writes

--- app/json/writes/Todo.scala
+++ app/json/writes/Todo.scala
@@ -1,7 +1,10 @@
 package json.writes
 
-import play.api.libs.json.Json
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+
 import lib.model.Todo._
+import lib.model.Category
 
 // slaca -> json
 case class JsValueTodo(
@@ -9,12 +12,22 @@ case class JsValueTodo(
   body:          String,
   note:          Option[String],
   status:        Short,
-  categoryName:  String,
+  categoryId:    Option[Long],
+  categoryName:  Option[String],
+  categoryColor: Option[Short]
 )
 
 object JsValueTodo {
 
-  implicit val jsValueTodo = Json.writes[JsValueTodo]
+  implicit val jsValueTodo: Writes[JsValueTodo] = (
+    (JsPath \ "id"           ).write[Long]             and
+    (JsPath \ "body"         ).write[String]           and
+    (JsPath \ "note"         ).write[Option[String]]   and
+    (JsPath \ "status"       ).write[Short]            and
+    (JsPath \ "categoryId"   ).write[Option[Long]]     and
+    (JsPath \ "categoryName" ).write[Option[String]]     and
+    (JsPath \ "categoryColor").write[Option[Short]]
+  )
 
   /*  @NOTE
    *  case classと異なる形のWritesが欲しい場合は、整形用のメソッドの自作が必要
